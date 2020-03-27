a43d72e855473e21047ff3231da23081affca98f
Tupiet <Tupiet@users.noreply.github.com>
2020-03-27 16:52:23 +0100

    Añadida la aplicación

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace WeExtract
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CompressButton = new System.Windows.Forms.Button();
+            this.zipName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.folderName = new System.Windows.Forms.TextBox();
+            this.DecompressButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CompressButton
+            // 
+            this.CompressButton.Font = new System.Drawing.Font("Avenir Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CompressButton.Location = new System.Drawing.Point(299, 128);
+            this.CompressButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CompressButton.Name = "CompressButton";
+            this.CompressButton.Size = new System.Drawing.Size(152, 46);
+            this.CompressButton.TabIndex = 0;
+            this.CompressButton.Text = "Comprimir";
+            this.CompressButton.UseVisualStyleBackColor = true;
+            this.CompressButton.Click += new System.EventHandler(this.CompressButton_Click);
+            // 
+            // zipName
+            // 
+            this.zipName.Location = new System.Drawing.Point(347, 77);
+            this.zipName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zipName.Name = "zipName";
+            this.zipName.Size = new System.Drawing.Size(306, 29);
+            this.zipName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 62);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "WeExtract";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(226, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(284, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "La nueva forma de extraer archivos";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.zipName);
+            this.groupBox1.Controls.Add(this.CompressButton);
+            this.groupBox1.Location = new System.Drawing.Point(30, 72);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(673, 195);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Avenir Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 80);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(322, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "¿Qué nombre quieres que tenga el archivo?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(292, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 38);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Comprimir";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.folderName);
+            this.groupBox2.Controls.Add(this.DecompressButton);
+            this.groupBox2.Location = new System.Drawing.Point(30, 286);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(673, 199);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(276, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(190, 38);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Descomprimir";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Avenir Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(17, 79);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(323, 21);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "¿Qué nombre quieres que tenga la carpeta?";
+            // 
+            // folderName
+            // 
+            this.folderName.Location = new System.Drawing.Point(347, 79);
+            this.folderName.Margin = new System.Windows.Forms.Padding(4);
+            this.folderName.Name = "folderName";
+            this.folderName.Size = new System.Drawing.Size(306, 29);
+            this.folderName.TabIndex = 1;
+            // 
+            // DecompressButton
+            // 
+            this.DecompressButton.Font = new System.Drawing.Font("Avenir Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DecompressButton.Location = new System.Drawing.Point(299, 128);
+            this.DecompressButton.Margin = new System.Windows.Forms.Padding(4);
+            this.DecompressButton.Name = "DecompressButton";
+            this.DecompressButton.Size = new System.Drawing.Size(152, 46);
+            this.DecompressButton.TabIndex = 0;
+            this.DecompressButton.Text = "Descomprimir";
+            this.DecompressButton.UseVisualStyleBackColor = true;
+            this.DecompressButton.Click += new System.EventHandler(this.DecompressButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 504);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Avenir Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "WeExtract";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CompressButton;
+        private System.Windows.Forms.TextBox zipName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox folderName;
+        private System.Windows.Forms.Button DecompressButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+
+namespace WeExtract
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CompressButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Primero, selecciona la carpeta que deseas comprimir.\nLuego, la ubicación deseada.");
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            FolderBrowserDialog file = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                Console.WriteLine("Hola");
+            }
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                Console.WriteLine("Hola");
+            }
+
+            if (zipName.Text == "")
+            {
+                ZipFile.CreateFromDirectory(fbd.SelectedPath, file.SelectedPath + "/" + "archivoComprimido" + ".zip");
+                MessageBox.Show("¡Su carpeta fue comprimida con éxito!", "Operación completada");
+            }
+            else
+            {
+                ZipFile.CreateFromDirectory(fbd.SelectedPath, file.SelectedPath + "/" + zipName.Text + ".zip");
+                MessageBox.Show("¡Su carpeta fue comprimida con éxito!", "Operación completada");
+            }
+
+        }
+
+        private void DecompressButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Primero, selecciona el archivo que deseas descomprimir.\nLuego, la ubicación deseada.");
+            OpenFileDialog file = new OpenFileDialog();
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            using (file)
+            {
+                file.Filter = "zip files (*.zip)|*.zip|All files (*.*)|*.*";
+                if (file.ShowDialog() == DialogResult.OK)
+                {
+                    Console.WriteLine("Hola");
+                }
+            }
+            
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                Console.WriteLine("Hola");
+            }
+
+            if (folderName.Text == "")
+            {
+                ZipFile.ExtractToDirectory(file.FileName, fbd.SelectedPath + "/" + "carpetaDescomprimida");
+                MessageBox.Show("¡Su archivo fue descomprimido con éxito!", "Operación completada");
+            }
+            else
+            {
+                ZipFile.ExtractToDirectory(file.FileName, fbd.SelectedPath + "/" + folderName.Text);
+                MessageBox.Show("¡Su archivo fue descomprimido con éxito!", "Operación completada");
+            }
+        }
+    }
+}
